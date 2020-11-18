da4fcc126cde06ef01f5b97df495c1205d2282e1
cyla00 <ikhayam000@protonmail.com>
2020-11-18 19:47:28 +0100

    creation of project and app

---
+++ main.py
@@ -0,0 +1,61 @@
+import os
+import sys
+import time
+
+urls = 'urls.py'
+templates = 'templates'
+
+def app():
+    app_name = input('choose an app name: ')
+    if os.path.isdir('./' + app_name):
+        print(app_name + ' exists')
+    else:
+        comando_app = 'python manage.py startapp ' + app_name
+        os.system(comando_app)
+        file = open(urls, 'w+')
+        file.write('./' + app_name)
+        os.chdir(app_name)
+        os.mkdir(templates)
+        os.chdir(templates)
+        inner_templates = app_name
+        os.mkdir(inner_templates)
+        quit()
+
+def project():
+    print('choose a name for your project: ')
+    proj_name = input()
+    if os.path.isdir('./' + proj_name):
+        print('ERROR: folder "' + proj_name + '" exists')
+        project()
+    else:
+        comando = 'django-admin startproject ' + proj_name
+        try:
+            os.system(comando)
+            print('creating file...')
+            time.sleep(2)
+            os.chdir(proj_name)
+        except ValueError:
+            print('an error occurred...')
+
+        
+        no = 'n'
+        yes = 'y'
+        question = input('create a superuser? [y][n] choose no: ') 
+        if question == no:
+            print('superuser NOT CREATED')
+            try:
+                app()
+            except ValueError:
+                print('an error occurred...')
+        elif question == yes:
+            admin_creation = 'python manage.py createsuperuser'
+            os.system(admin_creation)
+            print('superuser CREATED')
+            try:
+                app()
+            except ValueError:
+                print('an error occurred...')
+        else:
+            print('ERROR occurred')
+
+project()
